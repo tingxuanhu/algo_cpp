e78e511c51a53dc5654fccd84d9e21354a5222a9
tingxuanhu <paperplant2022@gmail.com>
2023-03-13 09:38:04 +0800

    update

---
+++ tree/traversal.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <deque>
+
+using namespace std;
+
+class TreeNode
+{
+    int value;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x): value(x) {left = nullptr; right = nullptr;}
+
+};
+
+
+/*
+用栈实现前中后序遍历（迭代，非递归）
+*/ 
+void preorderTraversal(TreeNode *root)
+{
+    
+
+}
+
